a9d1bf022806c9484b5fd87baaad20caab92916e
Betty Kemboi <betty.j.kemboi@gmail.com>
2020-10-18 13:37:02 -0500

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <h1>Welcome to ProShop</h1>
+      </>
+    </div>
+  );
+}
+
+export default App;
