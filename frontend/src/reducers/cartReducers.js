5407a157fab7002f75527e058e1857613fe98286
Betty Kemboi <betty.j.kemboi@gmail.com>
2020-10-21 17:06:01 -0500

    Added to cart functionality

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,28 @@
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../types/cartTypes";
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      const item = action.payload;
+
+      const itemExists = state.cartItems.find(
+        (x) => x.product === item.product
+      );
+
+      if (itemExists) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((x) =>
+            x.product === itemExists.product ? item : x
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, item],
+        };
+      }
+    default:
+      return state;
+  }
+};
