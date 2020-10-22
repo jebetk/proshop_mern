9272c1f3161c5707a91552a912a94833dfa94f1e
Betty Kemboi <betty.j.kemboi@gmail.com>
2020-10-22 00:52:08 -0500

    Custom auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,29 @@
 import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
-// @desc    Authenticate user & get token
-// @route   POST api/users.login
+// @desc    Get user profile / logged in user
+// @route   GET api/users/profile
 // @ access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+// @desc    Authenticate user & get token
+// @route   POST api/users/login
+// @ access Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
@@ -15,7 +35,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
@@ -23,4 +43,4 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+export { authUser, getUserProfile };
